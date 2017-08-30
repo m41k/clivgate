71260150ab3cde8dacce6a584bda51d3c7ffe0e4
m41k <maik.alberto@hotmail.com>
2017-08-29 21:34:35 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install yad -y
+apt-get install yad -y &
 apt-get install openvpn -y
 wget https://raw.githubusercontent.com/m41k/clivgate/master/clivgate
 chmod +x clivgate
